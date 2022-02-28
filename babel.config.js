70a49c848afa1af246ddf1958055eb877b0b05c9
vuhpquang <vuhpquang@gmail.com>
2022-02-28 09:39:20 +0700

    config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.json'],
+        alias: {
+          src: './src',
+          components: './src/components',
+        },
+      },
+    ],
+  ],
 };
